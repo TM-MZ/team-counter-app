210db927118c57f24fc8877fcc31c3e7d2c6060d
TM-MZ <115058763+TM-MZ@users.noreply.github.com>
2022-11-25 16:15:09 +0900

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    sum: 0,
+    lastTeam:'',
   },
   getters: {
   },
   mutations: {
+    incrementSum(state, payload) {
+      state.sum++;
+      state.lastTeam = payload;
+      
+    }
   },
   actions: {
   },
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h2>current team: {{ name }}</h2>
+        <button @click="increase">counter</button>
+        <p class="sum">{{$store.state.sum}}</p>
+        <p>Last team name clicked: {{$store.state.lastTeam}}</p>
+        <router-link :to="`/`">戻る</router-link>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            teams: [
+                {
+                    id: "c36136ef-552f-4dc9-84b6-65ecb8574e81",
+                    name: "Team_A",
+                },
+                {
+                    id: "ed0459f5-716d-4ef4-99ba-5dc750dfe0d0",
+                    name: "Team_B",
+                },
+                {
+                    id: "60d0d5d5-8ce9-41d9-a65b-13a090c10ae1",
+                    name: "Team_C",
+                },
+            ],
+        };
+    },
+    methods:{
+      increase(){
+        this.$store.commit('incrementSum',this.name);
+      }
+    },
+    props: ["name",],
+};
+</script>
+
+<style scoped>
+  .sum{
+    font-size:28px;
+    font-weight: bold;
+  }
+</style>
